080c6787d18325660bca1bfc2ed0544563cf8581
cars0106 <cars0106@naver.com>
2020-03-07 02:13:35 +0900

    Counter App by React Native
    Signed-off-by: cars0106 <cars0106@naver.com>

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Fragment } from 'react';
+import Counter from './Screens/Counter';
+import Styled from 'styled-components/native';
+
+const Container = Styled.View`
+  flex: 1;
+  background-color: #EEE;
+`;
+
+const App = () => {
+  return (
+    <Container>
+      <Counter title="This is a Counter App" initValue={5} />
+    </Container>
+  );
+};
+
+export default App;
